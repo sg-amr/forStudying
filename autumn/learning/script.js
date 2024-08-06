a457ad2fb5914e94a79dc9706c93f20c7f2b6fbc
Autumn0016 <autumn10100016@gmail.com>
2024-08-06 19:04:22 +0900

    renshuu

--- autumn/learning/script.js
+++ autumn/learning/script.js
@@ -1,3 +1,36 @@
 document.querySelector('p').textContent=('Hello World!');
 //window.alert('Hello World!!!');
 console.log('helloworld');
+
+// alert('hello world');[]
+
+const list = [1,2,3,4,5,6,7,8,9]
+
+console.log("list 0 is" + list[0]);
+console.log("list 1 is" + list[1]);
+console.log("list 2 is" + list[2]);
+
+const object = {
+    key1: "key1 content",
+    key2: "key2 content",
+    key3: "key3 content"
+}
+console.log("key1 is " + object.key1);
+console.log("key2 is " + object.key2);
+console.log("key3 is " + object.key3);
+
+true; 
+false;
+const a = 10 == 20
+if (a) {
+    // trueの時の処理
+    console.log("output is true")
+} else {
+    // false
+    console.log("output is false")
+}
+
+let variable;
+console.log(variable);
+variable = 1;
+console.log(variable);

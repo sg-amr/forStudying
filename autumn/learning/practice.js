375895121ff881cbf0828b31372c5f268dddc1c9
Autumn0016 <autumn10100016@gmail.com>
2024-08-06 21:39:04 +0900

    s

--- autumn/learning/practice.js
+++ autumn/learning/practice.js
@@ -1,6 +1,24 @@
+const numberA =1
+const numberB=2
+console.log (numberA + numberB)
 
+const object = {
+                str1:"hello",
+                str2:"world"
+                }
+            document.write (object.str1 + object.str2);
 
-const element = document.getElementById("idName");
+const list = [1,2,3,4,5];
+               
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+}
 
-element.innerHTML += "<h1>OUTPUT</h1>";
-
+var str = "Autumn"
+ document.write (str.length)
+const istrue = 10 == 20;
+if(istrue){
+    document.getElementById("example").textContent = "true";
+}else{
+    document.getElementById("example").textContent = "false";
+}

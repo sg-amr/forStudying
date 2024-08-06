3b15968f1c3f588b32361600967b1efaca789e7d
sonneko <sonneko27@gmail.com>
2024-08-06 23:10:12 +0900

    j

--- autumn/study js/practice.js
+++ autumn/study js/practice.js
@@ -1,2 +1,13 @@
 
 
+// const element = document.getElementById("idName");
+
+// element.innerHTML += "<h1>OUTPUT</h1>";
+
+
+let a = "first";
+
+a += "second";
+// a = a + "second";
+
+console.log(a);
